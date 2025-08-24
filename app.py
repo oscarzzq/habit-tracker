77a0f541d1bc903993cd4814366dc28a19fe9992
Oscar Zhang <oscarzhangzzq@gmail.com>
2025-08-23 17:35:47 -0700

    Ensure database file exists before connection

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+open("habits.db", "a").close()
+
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///habits.db")
 # Create tables if they don't exist
